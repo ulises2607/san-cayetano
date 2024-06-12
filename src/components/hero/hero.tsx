775429c58e542e8d966f65df59e435728ecc6edd
Ulises <ulisescappa@gmail.com>
2024-06-12 13:20:33 -0300

    Agregando Carousel con flowbite y formulario de curso.

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,11 +1,8 @@
-"use client";
-import Image from "next/image";
 import Carousel from "./carousel/Carousel";
 
 const HeroSection = () => {
   return (
     <div>
-      Hero
       <Carousel />
     </div>
   );
